764b4aef7067d835a8db090756737e376893d920
Badma Ochirov <howlotus@yandex.ru>
2022-09-26 14:31:17 +0300

    .gitignore complemented

--- src/exceptions/custom_errors.py
+++ src/exceptions/custom_errors.py
@@ -1,14 +1,14 @@
-class CustomError(Exception):
+class CustomError:
     """Base exception for custom errors"""
 
+    class InvalidArgsError(Exception):
+        """This exception raises, when user enters many arguments"""
 
-class NotNumberError(CustomError):
-    """This exception raises, when user enters not a number"""
+    class NotNumberError(Exception):
+        """This exception raises, when user enters not a number"""
 
+    class OccupiedCellError(Exception):
+        """This exception raises, when user tries to enter an occupied cell"""
 
-class OccupiedCellError(CustomError):
-    """This exception raises, when user tries to enter an occupied cell"""
-
-
-class OutOfRangeError(CustomError):
-    """This exception raises, when user enters a number out of range from 1 to 9"""
+    class OutOfRangeError(Exception):
+        """This exception raises, when user enters a number out of range from 1 to 9"""
--- src/tictactoe.py
+++ src/tictactoe.py
@@ -1,4 +1,6 @@
 """This module runs a game"""
+from exceptions.custom_errors import CustomError
+from functions.is_str_a_number import is_number
 
 
 class TicTacToeGame:
@@ -11,34 +13,30 @@ class TicTacToeGame:
         """This method prints board for TicTacToe"""
 
         board = self.board
-        print("-------------")
-        for i in range(3):
-            print(f"| {board[i * 3]} | {board[1 + i * 3]} | {board[2 + i * 3]} |\n-------------")
 
-    def validate_input(self, player_number):
+        print("=============")
+        for i in range(3):
+            print(f"| {board[i * 3]} | {board[1 + i * 3]} | {board[2 + i * 3]} |")
+        print("=============")
+
+    def validate_input(self, input_value):
         """This method takes input and checks its validation"""
 
-        player_token = "X" if player_number == 1 else "O"
+        if (len(input_value.split())) != 1:
+            raise CustomError.InvalidArgsError
 
-        print(f"\x1b[6;30;43mIt's turn of Player {player_number} ({player_token})."
-              f"\x1b[0m\n\x1b[6;30;43mEnter a number:\x1b[0m")
+        if not is_number(input_value):
+            raise CustomError.NotNumberError
 
-        is_answer_valid = False
-        while not is_answer_valid:
-            player_answer = input()
-            try:
-                player_answer = int(player_answer)
-            except ValueError:
-                print("\x1b[6;30;41mIncorrect input! It isn't a number.\x1b[0m")
-                continue
-            if player_answer in range(1, 10):
-                if str(self.board[player_answer - 1]) not in "XO":
-                    self.board[player_answer - 1] = player_token
-                    is_answer_valid = True
-                else:
-                    print("\x1b[6;30;41mIncorrect input! This cell is occupied.\x1b[0m")
-            else:
-                print("\x1b[6;30;41mIncorrect input! Enter a number in range from 1 to 9.\x1b[0m")
+        player_answer = int(input_value) - 1
+
+        if player_answer not in range(9):
+            raise CustomError.OutOfRangeError
+
+        if str(self.board[player_answer]) in "XO":
+            raise CustomError.OccupiedCellError
+
+        return player_answer
 
     def start_game(self):
         """This method implements the game from to start to end"""
@@ -46,26 +44,40 @@ class TicTacToeGame:
         message = ""
         counter = 0
         is_there_winner = False
+        self.show_board()
         while not is_there_winner:
-            self.show_board()
+            player_number = counter % 2 + 1
+            player_token = "X" if player_number == 1 else "O"
+            print(f"\x1b[6;30;43mIt's turn of Player {player_number} ({player_token})."
+                  f"\x1b[0m\n\x1b[6;30;43mEnter a number:\x1b[0m")
 
-            tmp = counter % 2 + 1
-            self.validate_input(tmp)
+            try:
+                self.board[self.validate_input(input())] = player_token
+            except CustomError.InvalidArgsError:
+                print("\x1b[6;30;41mIncorrect input! Invalid number of arguments.\x1b[0m")
+                continue
+            except CustomError.NotNumberError:
+                print("\x1b[6;30;41mIncorrect input! It isn't a number.\x1b[0m")
+                continue
+            except CustomError.OccupiedCellError:
+                print("\x1b[6;30;41mIncorrect input! This cell is occupied.\x1b[0m")
+                continue
+            except CustomError.OutOfRangeError:
+                print("\x1b[6;30;41mIncorrect input! Enter a number in range from 1 to 9.\x1b[0m")
+                continue
 
             counter += 1
+            self.show_board()
 
             if counter > 4:
                 winner = self.check_winner()
                 if winner:
-                    message = f"\x1b[6;30;42mPlayer {tmp} won!\x1b[0m"
+                    message = f"\x1b[6;30;42mPlayer {player_number} won!\x1b[0m"
                     is_there_winner = True
-                    continue
-
-                if counter == 9:
+                elif counter == 9:
                     message = "\x1b[6;30;45mDraw!\x1b[0m"
                     is_there_winner = True
 
-        self.show_board()
         print(message)
 
     def check_winner(self):
