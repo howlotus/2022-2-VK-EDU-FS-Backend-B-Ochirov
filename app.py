15c1663c62d1cfd8f52ddbb611aa44bd3cdf39bc
Badma Ochirov <howlotus@yandex.ru>
2022-10-01 21:02:15 +0300

    2 tasks are done

---
+++ app.py
@@ -0,0 +1,19 @@
+def app(environ, start_response):
+    """Simplest possible application object"""
+    data = b'Hello, World!\n'
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain'),
+        ('Content-Length', str(len(data)))
+    ]
+    start_response(status, response_headers)
+    return iter([data])
+
+def process_http_request(environ, start_response):
+    status = '200 OK'
+    response_headers = [
+        ('Content-type', 'text/plain; charset=utf-8'),
+    ]
+    start_response(status, response_headers)
+    text = 'Here be dragons'.encode('utf-8')
+    return [text]
