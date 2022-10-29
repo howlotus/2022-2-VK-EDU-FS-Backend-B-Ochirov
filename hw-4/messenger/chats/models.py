49167832857f9bf3d5424212a472de45f7feff3b
Badma Ochirov <howlotus@yandex.ru>
2022-10-29 22:41:20 +0300

    ups

--- hw-4/messenger/chats/models.py
+++ hw-4/messenger/chats/models.py
@@ -5,9 +5,12 @@ from users.models import User
 
 # Models for chats and messages
 class Chat(models.Model):
-    id = models.AutoField(primary_key=True, verbose_name='Идентификатор чата')
-    first_user = models.ForeignKey(User, null=True, verbose_name='Идентификатор пользователя 1',
-                                   on_delete=models.CASCADE) # reference to a new table
+    first_user = models.ForeignKey(
+        User,
+        null=True,
+        verbose_name='Идентификатор пользователя 1',
+        on_delete=models.CASCADE
+    )  # reference to a new table
     creation_datetime = models.DateTimeField(default=now, verbose_name='Дата и время создания чата')
 
     class Meta:
@@ -15,19 +18,19 @@ class Chat(models.Model):
 
 
 # class ChatMember(models.Model):
-    # chat-fk, user-fk, date +
+# chat-fk, user-fk, date +
 
 
-class Message(models.Model): # everywhere to do related_name
+class Message(models.Model):  # everywhere to do related_name
     # убрать айди везде, primary key само создается
 
-    id = models.AutoField(primary_key=True, verbose_name='Идентификатор сообщения')
     chat = models.ForeignKey(
         Chat,
         null=True,
         verbose_name='Идентификатор чата',
         on_delete=models.CASCADE,
-        related_name='messages')
+        related_name='messages'
+    )
     sender = models.ForeignKey(
         User,
         null=True,
--- hw-4/messenger/users/models.py
+++ hw-4/messenger/users/models.py
@@ -1,9 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
 
 
 # Models for user
-class User(models.Model): # to AbstractUser
-    id = models.AutoField(primary_key=True, verbose_name='Идентификатор пользователя')
+class User(AbstractUser):
     phone_number = models.TextField(verbose_name='Номер телефона')
     username = models.TextField(unique=True, verbose_name='Имя пользователя')
 
@@ -11,12 +11,15 @@ class User(models.Model): # to AbstractUser
         verbose_name = 'Пользователь'
 
 
-class UserInfo(models.Model): # UserProfile
-    id = models.AutoField(primary_key=True, verbose_name='Идентификатор записи о пользователе')
+class UserProfile(models.Model):  # UserProfile
     name = models.TextField(verbose_name='Имя пользователя')
     info = models.TextField(verbose_name='Краткая информация о пользователе')
     creation_datetime = models.DateTimeField(verbose_name='Дата и время создания пользователя')
-    user = models.OneToOneField(User, verbose_name='Идентификатор пользователя', on_delete=models.CASCADE)
+    user = models.OneToOneField(
+        User,
+        verbose_name='Идентификатор пользователя',
+        on_delete=models.CASCADE
+    )
 
     class Meta:
         verbose_name = 'Информация о пользователе'
